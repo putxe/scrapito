241ee8abd288e19c773ae477def4d6b2207adb3c
poutch <damien.pucheu@hotmail.fr>
2021-04-30 11:03:25 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,11 +2,12 @@ window.addEventListener("message", receiveMessage, false)
 
 function receiveMessage(message) {
   if (message.origin !== "https://www.linkedin.com") return
-  if (!message.link) return
-  if (!message.link.startsWith("/in/")) return
-  if (!message.type) return
-  if (!message.type !== "ROUTE_REDIRECT") return
-  const link = message.link
+  if (!message.data) return
+  if (!message.data.link) return
+  if (!message.data.link.startsWith("/in/")) return
+  if (!message.data.type) return
+  if (!message.data.type !== "ROUTE_REDIRECT") return
+  const link = message.data.link
   const app = window.Ember.A(window.Ember.Namespace.NAMESPACES).filter(n => n.name === "extended")[0]
   const r = app.__container__.lookup("service:router")
   r.transitionTo(link)
