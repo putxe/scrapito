3a06fb18c274192245776cf20af6a7145dbd0392
poutch <damien.pucheu@hotmail.fr>
2021-04-30 11:07:48 +0200

    Update test.js

--- test.js
+++ test.js
@@ -6,9 +6,9 @@ function receiveMessage(message) {
   if (!message.data.link) return
   if (!message.data.link.startsWith("/in/")) return
   if (!message.data.type) return
-  if (!message.data.type !== "ROUTE_REDIRECT") return
-  var link = message.data.link
-  var app = window.Ember.A(window.Ember.Namespace.NAMESPACES).filter(n => n.name === "extended")[0]
-  var r = app.__container__.lookup("service:router")
+  if (message.data.type !== "ROUTE_REDIRECT") return
+  const link = message.data.link
+  const app = window.Ember.A(window.Ember.Namespace.NAMESPACES).filter(n => n.name === "extended")[0]
+  const r = app.__container__.lookup("service:router")
   r.transitionTo(link)
 }
